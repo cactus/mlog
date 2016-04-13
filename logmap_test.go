02d10093e88a61c95d5c4b991ba53a9085ce9a0a
elij <elij@wundrd.net>
2016-04-12 23:31:17 -0700

    add another test

---
+++ logmap_test.go
@@ -0,0 +1,30 @@
+package mlog
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkLogMapWriteTo(b *testing.B) {
+	m := Map{}
+	for i := 1; i <= 100; i++ {
+		m[randString(6)] = randString(10)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.WriteTo(ioutil.Discard)
+	}
+}
+
+func TestLogMapWriteTo(t *testing.T) {
+	m := Map{"test": "this is \"a test\" of \t some \n a"}
+	buf := &bytes.Buffer{}
+	m.WriteTo(buf)
+	n := `test="this is \"a test\" of \t some \n a"`
+	l := buf.String()
+	assert.Equal(t, n, l, "did not match")
+
+}
