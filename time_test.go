017ea9c380c7edd58019838b99fdf49a5098f814
elij <elij@wundrd.net>
2016-05-05 13:34:02 -0700

    cleanup

--- time_test.go
+++ time_test.go
@@ -8,32 +8,38 @@ import (
 )
 
 func TestTime(t *testing.T) {
+	loc := time.FixedZone("PDT", -25200)
+	cases := []struct {
+		F FlagSet
+		T time.Time
+		R string
+	}{
+		{
+			Lnanoseconds,
+			time.Date(2016, time.November, 1, 2, 3, 4, 5, loc),
+			`2016-11-01T02:03:04.000000005-07:00`,
+		},
+		{
+			Lnanoseconds,
+			time.Date(2016, time.January, 11, 12, 13, 14, 15, time.UTC),
+			`2016-01-11T12:13:14.000000015Z`,
+		},
+		{
+			Lmicroseconds,
+			time.Date(2016, time.November, 1, 2, 3, 4, 5000, loc),
+			`2016-11-01T02:03:04.000005-07:00`,
+		},
+		{
+			Lmicroseconds,
+			time.Date(2016, time.January, 11, 12, 13, 14, 15000, time.UTC),
+			`2016-01-11T12:13:14.000015Z`,
+		},
+	}
+
 	b := &sliceBuffer{make([]byte, 0, 1024)}
-	loc, _ := time.LoadLocation("America/Los_Angeles")
-
-	// test nanos
-	tm := time.Date(2016, time.November, 1, 2, 3, 4, 5, loc)
-	writeTime(b, &tm, Lnanoseconds)
-	assert.Equal(t, `2016-11-01T02:03:04.000000005-07:00`, b.String(),
-		"time written incorrectly")
-
-	b.Truncate(0)
-	tm = time.Date(2016, time.January, 11, 12, 13, 14, 15, time.UTC)
-	writeTime(b, &tm, Lnanoseconds)
-	assert.Equal(t, `2016-01-11T12:13:14.000000015Z`, b.String(),
-		"time written incorrectly")
-
-	// test micros
-	b.Truncate(0)
-	tm = time.Date(2016, time.November, 1, 2, 3, 4, 5000, loc)
-	writeTime(b, &tm, Lmicroseconds)
-	assert.Equal(t, `2016-11-01T02:03:04.000005-07:00`, b.String(),
-		"time written incorrectly")
-
-	b.Truncate(0)
-	tm = time.Date(2016, time.January, 11, 12, 13, 14, 15000, time.UTC)
-	writeTime(b, &tm, Lmicroseconds)
-	assert.Equal(t, `2016-01-11T12:13:14.000015Z`, b.String(),
-		"time written incorrectly")
-
+	for _, tc := range cases {
+		b.Truncate(0)
+		writeTime(b, &(tc.T), tc.F)
+		assert.Equal(t, tc.R, b.String(), "time written incorrectly")
+	}
 }
